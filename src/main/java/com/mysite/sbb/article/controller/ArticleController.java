c484a304f1e21c58cf752a69ad0095030d21cbc1
chuchu02 <rlaxogns73@gmail.com>
2022-07-07 15:05:13 +0900

    게시물 수정

--- src/main/java/com/mysite/sbb/article/controller/ArticleController.java
+++ src/main/java/com/mysite/sbb/article/controller/ArticleController.java
@@ -35,4 +35,20 @@ public class ArticleController {
         Optional<Article> article = articleRepository.findById(id);
         return article.orElse(null);
     }
+
+    @RequestMapping("/doModify")
+    @ResponseBody
+    public Article showModify(long id, String title, String body) {
+        Article article = articleRepository.findById(id).get();
+        if (title != null) {
+            article.setTitle(title);
+        }
+
+        if (body != null) {
+            article.setBody(body);
+        }
+
+        articleRepository.save(article);
+        return article;
+    }
 }
