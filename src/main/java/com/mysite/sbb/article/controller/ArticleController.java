26b5936f9dac17e2bdc44004070e679a30561987
chuchu02 <rlaxogns73@gmail.com>
2022-07-07 14:51:33 +0900

    id값을 조건으로 받는 게시물 단건 조회기능구현

--- src/main/java/com/mysite/sbb/article/controller/ArticleController.java
+++ src/main/java/com/mysite/sbb/article/controller/ArticleController.java
@@ -5,9 +5,11 @@ import com.mysite.sbb.article.domain.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/usr/article")
@@ -29,8 +31,8 @@ public class ArticleController {
 
     @RequestMapping("/detail")
     @ResponseBody
-    public Article showArticleDetaik() {
-        return articleRepository.findById(1L).get();
+    public Article showArticleDetail(@RequestParam long id) {
+        Optional<Article> article = articleRepository.findById(id);
+        return article.orElse(null);
     }
-
 }
